f7593adac5219fda59bfcde115eef908d0c2acca
zulnurdiana <zulnurdiana@gmail.com>
2022-11-21 22:09:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>The Movies</h1>
+        <input type="text" placeholder="Search your fav movie" class="Movie-search" />
+        <div class="Movie-container">
+          <div class="Movie-wrapper">
+            <div class="Movie-title">test</div>
+            <img  class="Movie-image" src=""  alt=""/>
+            <div class="Movie-release">test</div>
+            <div className="Movie-rating">test</div>
+          </div>
+        </div>
       </header>
     </div>
   );
